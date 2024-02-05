1ad63e0b213a82d5824299a316397b2f69d38654
Sulav Shrestha <sulavshrestha2323@gmail.com>
2024-02-05 22:30:42 +0545

    add model for todos

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { env } from "./utils/envValidation";
-import { connectDb } from "./db";
+import { connectDb } from "./config/db.config";
 import { server } from "./app";
 
 const PORT = env.PORT || 8000;
